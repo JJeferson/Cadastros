ed409236002fec5cdcaa985c84756977716a3437
JJeferson <48735241+JJeferson@users.noreply.github.com>
2021-06-02 18:22:14 -0300

    Implementados ajustes nos testes Junit com Mockito

---
+++ src/test/java/com/Cadastros/TestaCidade.java
@@ -0,0 +1,26 @@
+package com.Cadastros;
+
+
+import com.Cadastros.model.Cidade;
+import com.Cadastros.repository.CidadeRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.ArgumentMatchers.notNull;
+import static org.mockito.Mockito.mock;
+
+@SpringBootTest
+public class TestaCidade {
+    @Mock
+    CidadeRepository cidadeRepository ;
+    @Test
+    void  testaSave() {
+        Cidade cidadeTeste  = mock(Cidade.class);
+        Cidade  salvaCidade  = cidadeRepository.save(cidadeTeste);
+        assertEquals(notNull(),salvaCidade);
+    }
+
+}
---
+++ src/test/java/com/Cadastros/TestaCliente.java
@@ -0,0 +1,25 @@
+package com.Cadastros;
+
+import com.Cadastros.model.Cliente;
+import com.Cadastros.repository.ClienteRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.notNull;
+import static org.mockito.Mockito.mock;
+
+
+
+@SpringBootTest
+public class TestaCliente {
+    @Mock
+    ClienteRepository clienteRepository ;
+    @Test
+    void  testaSaveCliente() {
+        Cliente clienteTeste  = mock(Cliente.class);
+        Cliente  salvaCliente  = clienteRepository.save(clienteTeste);
+        assertEquals(notNull(),salvaCliente);
+    }
+}
